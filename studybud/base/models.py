2865f47e9403b3b30efd124373673707328b43f3
bibonidze <mozoliwe@gmail.com>
2023-01-13 21:44:41 +0300

    kaka

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,15 +1,40 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    topic = models.ForeignKey(Topic,  on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     #participants =
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    # Когда создается или обновляется Room выдает время когда это было сделано
 
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # 1.30.00
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    # 1.27.00 CASCADE Определает что делать с пользователями
+    # после того как удалилсь Room
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
